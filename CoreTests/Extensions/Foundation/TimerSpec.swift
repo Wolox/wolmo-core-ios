68a1f801a14cfd21bfae15dd93a942865938da26
danielaRiesgo <danielap.riesgo@gmail.com>
2017-03-22 17:09:01 -0300

    Updated timer tests

--- CoreTests/Extensions/Foundation/TimerSpec.swift
+++ CoreTests/Extensions/Foundation/TimerSpec.swift
@@ -20,10 +20,10 @@ public class TimerSpec: QuickSpec {
         
         afterEach {
             timer!.invalidate()
-            timer = nil
+            timer = .none
         }
         
-        describe("#schedule(delay:)") {
+        describe("#schedule(withDelay:)") {
             
             it("should trigger handler after delay") { waitUntil(timeout: 2) { done in
                 timer = Timer.schedule(withDelay: 1) { _ in
@@ -32,7 +32,7 @@ public class TimerSpec: QuickSpec {
             }}
         }
         
-        describe("#schedule(repeat:)") {
+        describe("#schedule(repeatInterval:)") {
             
             it("should repeteadly trigger handler after delay") { waitUntil(timeout: 5) { done in
                 var timesCalled = 0
