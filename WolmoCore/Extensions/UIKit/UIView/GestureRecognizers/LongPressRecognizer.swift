8ec11fdd367598fa51fc41bd0f15911d4ee13bf1
Gabriel Mazzei <gmazzei91@gmail.com>
2018-12-26 11:59:46 -0300

    Comments fixed

--- WolmoCore/Extensions/UIKit/UIView/GestureRecognizers/LongPressRecognizer.swift
+++ WolmoCore/Extensions/UIKit/UIView/GestureRecognizers/LongPressRecognizer.swift
@@ -35,10 +35,10 @@ public extension UIView {
     /**
      Adds a long-press gesture recognizer that executes the closure when long pressed
      
-     - Parameter numberOfTapsRequired: The number of full taps required before the press for gesture to be recognized
-     - Parameter numberOfTouchesRequired: Number of fingers that must be held down for the gesture to be recognized
-     - Parameter minimumPressDuration: Time in seconds the fingers must be held down for the gesture to be recognized
-     - Parameter allowableMovement: Maximum movement in pixels allowed before the gesture fails. Once recognized (after minimumPressDuration) there is no limit on finger movement for the remainder of the touch tracking
+     - Parameter numberOfTapsRequired: The number of full taps required before the press for gesture to be recognized. Default is 0
+     - Parameter numberOfTouchesRequired: Number of fingers that must be held down for the gesture to be recognized. Default is 1
+     - Parameter minimumPressDuration: Time in seconds the fingers must be held down for the gesture to be recognized. Default is 0.5
+     - Parameter allowableMovement: Maximum movement in pixels allowed before the gesture fails. Once recognized (after minimumPressDuration) there is no limit on finger movement for the remainder of the touch tracking. Default is 10
      - Parameter action: The closure that will execute when the view is long pressed
      */
     public func addLongPressGestureRecognizer(numberOfTapsRequired: Int = 0,
