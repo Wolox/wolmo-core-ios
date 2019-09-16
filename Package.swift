e3c8a0ca5e2e1af8f767bbd38f212738e5bab738
diego quiros <diegoquiros@diegos-MacBook-Pro.local>
2019-09-16 18:58:40 -0300

    more package fixes, hope it works

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WolmoCore",
-            path: "../WolmoCore")
+            path: "./WolmoCore")
     ]
 )
